3f5811f22dda0941f6421e0b632e06111838a06e
LucasMoraisZago <lucasmoraiszago@hotmail.com>
2022-04-05 11:06:21 +0000

    trabalho redux

---
+++ projeto-avaliativo-react-redux/src/components/Desafio.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Card from "./Card"
+import { connect } from "react-redux"
+function Desafio(props){
+    return (
+        <Card title="DESAFIO REACT-REDUX" green>
+            <div>
+                <span>
+                    <span>Esse desafio é fácil d+ Dev’s do 6º ADS!!! </span>
+                </span>
+            </div>
+        </Card>
+    )
+}
+
+export default Desafio
